e0a91508a7ca81be71ba7a260ff61722716e443d
Jesse Li <jesseli@jesses-macbook-pro.local>
2018-11-28 17:04:45 -0600

    Initial Commit

---
+++ SchedulerPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchedulerPrototype
+//
+//  Created by Jesse Li on 11/28/18.
+//  Copyright © 2018 Jesse Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
